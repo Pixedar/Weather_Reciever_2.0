28c80a228ca2d73a11cede3c383d1cc540b6d842
Wiktor <pixedar@gmial.com>
2018-11-28 17:50:22 +0100

    fixed negative temp

--- ReceiveData.ino
+++ ReceiveData.ino
@@ -30,8 +30,8 @@ boolean getData() {
      return false;  
     }
     
-    if (data[0] > 100) {
-      tempO= (data[0]-200) + (float)(data[1]/100.0f);
+    if (data[0] > 50) {
+      tempO= (data[0]-50) + (float)(data[1]/100.0f);
       tempO*=-1;
     } else {
       tempO= data[0] + (float)(data[1]/100.0f);
